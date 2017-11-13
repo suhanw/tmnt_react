7bc2969be24fe3c91b1559e1148375742b47bdd9
Suhan Wijaya <suhanw@gmail.com>
2017-11-13 13:15:34 -0500

    bugfix

--- lib/components/reset.jsx
+++ lib/components/reset.jsx
@@ -26,11 +26,6 @@ class Reset extends React.Component {
         <div>
           Press 'S' to play again.
         </div>
-        <ul className="icons">
-          <li><a href="https://www.linkedin.com/in/suhanwijaya" target="_blank" className="linkedin"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
-          <li><a href="https://www.github.com/suhanw" target="_blank" className="github"><i className="fa fa-github" aria-hidden="true"></i></a></li>
-          <li><a href="https://angel.co/suhan-wijaya" target="_blank" className="angellist"><i className="fa fa-angellist" aria-hidden="true"></i></a></li>
-        </ul>
         <div>
           Reach me at <a className="email" href="mailto:suhanw@gmail.com">suhanw@gmail.com</a>
         </div>
@@ -44,6 +39,11 @@ class Reset extends React.Component {
             Visit my portfolio.
           </a>
         </div>
+        <ul className="icons">
+          <li><a href="https://www.linkedin.com/in/suhanwijaya" target="_blank" className="linkedin"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
+          <li><a href="https://www.github.com/suhanw" target="_blank" className="github"><i className="fa fa-github" aria-hidden="true"></i></a></li>
+          <li><a href="https://angel.co/suhan-wijaya" target="_blank" className="angellist"><i className="fa fa-angellist" aria-hidden="true"></i></a></li>
+        </ul>
         <small>
           Credit: sprite images from www.spriters-resource.com
         </small>
