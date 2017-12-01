032f1b508acc0375407cb66e96e2d08e3e8a8ec4
Suhan Wijaya <suhanw@gmail.com>
2017-12-01 10:23:46 -0500

    cleanup

--- lib/index.jsx
+++ lib/index.jsx
@@ -3,21 +3,8 @@ import React from 'react';
 import Root from './components/root';
 import configureStore from './store/store';
 
-// TESTING
-import {randomizeFoots} from './util/foots_util';
-import * as FootAction from './actions/foots_actions';
-// TESTING
-
 document.addEventListener('DOMContentLoaded', ()=>{
   const store = configureStore();
-
-  // TESTING
-  window.randomizeFoots = randomizeFoots;
-  window.dispatch = store.dispatch;
-  window.getState = store.getState;
-  window.resetFoots = FootAction.resetFoots;
-  // TESTING
-
   const rootDom = document.getElementById('root');
   ReactDOM.render(
     <Root store={store} />,
